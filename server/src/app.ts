27fa45906472238e3bf7899b1ac876c80a59bdd1
blinkelys <martinustomrenslemmen@gmail.com>
2026-04-24 10:02:24 +0200

    feat: backend init

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+
+
+// CORS Config
+import cors from 'cors';
+app.use(cors());
+    
+
+// Database connection
+import mongoose from 'mongoose';
+mongoose.connect('your_connection_string_here');
+      
+
+// Start the server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  
