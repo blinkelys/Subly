5b81d34a011ecfe19702e4975d1f562935625474
blinkelys <martinustomrenslemmen@gmail.com>
2026-04-27 09:52:22 +0200

    feat: auth work

--- client/src/router.ts
+++ client/src/router.ts
@@ -4,16 +4,35 @@ import About from './pages/About.vue'
 import Login from './pages/auth/Login.vue'
 import Register from './pages/auth/Register.vue'
 import Recover from './pages/auth/Recover.vue'
+import Dashboard from './pages/Dashboard.vue'
+import axios from "axios";
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
   { path: '/login', component: Login },
   { path: '/register', component: Register },
-  { path: '/recover', component: Recover }
+  { path: '/recover', component: Recover },
+  { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true }, }
 ]
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
+
+// Navigation guard for protected routes
+router.beforeEach(async (to, _from, next) => {
+  if (to.meta.requiresAuth) {
+    try {
+      await axios.get("/api/auth/me", { withCredentials: true });
+      next();
+    } catch (error) {
+      next("/login");
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
